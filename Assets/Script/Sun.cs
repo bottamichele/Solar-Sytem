2ba26815da2864afde9cb717b6241a308f04e8b5
Botta Michele <72565868+bottamichele@users.noreply.github.com>
2024-01-03 20:53:58 +0100

    Point Light does fix light within its range.

--- Assets/Script/Sun.cs
+++ Assets/Script/Sun.cs
@@ -9,9 +9,17 @@ public class Sun : SphericalCelestialObject
     [Tooltip("Temperature (in K)")]
     float temperature;              //Temperature (in Kelvin) of Sun.
 
+    /* ================================================== 
+     * =================== VARIABLES ====================
+     * ================================================== */
+    Light lightCmpt;
+
     protected new void Start()
     {
         base.Start();
-        this.transform.localScale /= 5;
+        transform.localScale /= 5;
+        
+        lightCmpt = GetComponent<Light>();
+        lightCmpt.range = ScaleConverter.ScaleLength(1.1f * 4.498408f * Mathf.Pow(10, 9) * (1.0f + 0.008678f));
     }
 }
